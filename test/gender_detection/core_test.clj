f63ba9c3fbc8b4c86fa07f981fa418c78c5dc686
Franco Mariluis <me@fmariluis.info>
2018-05-29 16:01:45 -0300

    adding tests (closes #2)

--- test/gender_detection/core_test.clj
+++ test/gender_detection/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [gender-detection.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest testing-female
+  (testing "Female spanish name"
+    (is (= (female? "Julia") true))))
+
+(deftest testing-male
+  (testing "Male spanish name"
+    (is (= (male? "Juan") true))))
+
+(deftest testing-find-gender-male
+  (testing "Find male spanish gender"
+    (is (= (find-gender "Pedro") :M))))
+
+(deftest testing-find-gender-female
+  (testing "Find female spanish gender"
+    (is (= (find-gender "Lucia") :F))))
