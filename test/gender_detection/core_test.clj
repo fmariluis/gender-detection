02e96d4e7b5ea2bc931afa76103711f6f9b98850
Franco Mariluis <me@fmariluis.info>
2018-05-30 23:05:14 -0300

    updating tests

--- test/gender_detection/core_test.clj
+++ test/gender_detection/core_test.clj
@@ -2,18 +2,42 @@
   (:require [clojure.test :refer :all]
             [gender-detection.core :refer :all]))
 
-(deftest testing-female
+(deftest testing-female-es
   (testing "Female spanish name"
-    (is (= (female? "Julia") true))))
+    (is (= (female? "Julia" :es) true))))
 
-(deftest testing-male
+(deftest testing-male-es
   (testing "Male spanish name"
-    (is (= (male? "Juan") true))))
+    (is (= (male? "Juan" :es) true))))
 
-(deftest testing-find-gender-male
+(deftest testing-find-gender-male-es
   (testing "Find male spanish gender"
-    (is (= (find-gender "Pedro") :M))))
+    (is (= (find-gender "Pedro" :es) :M))))
 
-(deftest testing-find-gender-female
+(deftest testing-find-gender-female-es
   (testing "Find female spanish gender"
-    (is (= (find-gender "Lucia") :F))))
+    (is (= (find-gender "Lucia" :es) :F))))
+
+(deftest testing-find-gender-undefined
+  (testing "Undefined gender returns nil"
+    (is (= (find-gender "Vorplax" :es) nil))))
+
+(deftest testing-find-language-undefined
+  (testing "Undefined language returns nil"
+    (is (= (find-gender "John" :ch) nil))))
+
+(deftest testing-female-en
+  (testing "Female English name"
+    (is (= (female? "Emma" :en) true))))
+
+(deftest testing-male-en
+  (testing "Male English name"
+    (is (= (male? "Peter" :en) true))))
+
+(deftest testing-find-gender-male-en
+  (testing "Find male English gender"
+    (is (= (find-gender "Clyde" :en) :M))))
+
+(deftest testing-find-gender-female-en
+  (testing "Find female spanish gender"
+    (is (= (find-gender "Beth" :en) :F))))
